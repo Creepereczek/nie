1e4a99eb87e9ff981c6c147ae64a8e9582619d7b
Creepereczek <Gabriel@DESKTOP-R7N52NA>
2023-06-06 20:26:34 +0200

    hunter fix

--- src/main/java/net/mcreator/nie/item/HunterItem.java
+++ src/main/java/net/mcreator/nie/item/HunterItem.java
@@ -3,20 +3,13 @@ package net.mcreator.nie.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.world.World;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.MusicDiscItem;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
-import net.minecraft.client.util.ITooltipFlag;
 
 import net.mcreator.nie.itemgroup.NieItemGroup;
 import net.mcreator.nie.NieModElements;
-
-import java.util.List;
 
 @NieModElements.ModElement.Tag
 public class HunterItem extends NieModElements.ModElement {
@@ -37,12 +30,6 @@ public class HunterItem extends NieModElements.ModElement {
 			super(0, NieModElements.sounds.get(new ResourceLocation("nie:hunter")),
 					new Item.Properties().group(NieItemGroup.tab).maxStackSize(1).rarity(Rarity.RARE));
 			setRegistryName("hunter");
-		}
-
-		@Override
-		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-			super.addInformation(itemstack, world, list, flag);
-			list.add(new StringTextComponent("nie"));
 		}
 	}
 }
